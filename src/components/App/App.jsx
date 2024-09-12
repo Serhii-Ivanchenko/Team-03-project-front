c9a2890dc8cab61b5cc114767a2b227e38001b8f
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-09-12 15:25:12 +0300

    create react project, add structure, dependencies, imgs, icons, fonts

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import SharedLayout from '../SharedLayout/SharedLayout.jsx'
+import './App.css'
+
+function App() {
+  
+  return (
+    <SharedLayout>
+      <p>Hello world!</p>
+    </SharedLayout>
+  )
+}
+
+export default App
