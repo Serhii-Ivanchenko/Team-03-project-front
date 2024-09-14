74e3e2de6121dccfcfe254673f4223c4bf87ecd9
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2024-09-14 11:14:11 +0300

    Delete Module (Style / Markup)

--- src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,9 +1,24 @@
-import "./DeleteWaterModal.module.css";
+import css from "./DeleteWaterModal.module.css";
 
 const DeleteWaterModal = () => {
-  return <div>
-    <p></p>
-  </div>;
+  return (
+    <div className={css.deleteModalContainer}>
+      <div className={css.deleteModalTextContainer}>
+        <h2 className={css.deleteModalTitle}>Delete entry</h2>
+        <p className={css.deleteModalText}>
+          Are you sure you want to delete the entry?
+        </p>
+      </div>
+      <div className={css.deleteModalBtnWrapper}>
+        <button className={css.deleteModalBtn}>
+          Delete
+        </button>
+        <button className={css.deleteModalCancelBtn}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteWaterModal;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,5 @@
-// export default function HomePage() {
-//   return <p>Hello world!</p>;
-// }
+export default function HomePage() {
+  return <p>Hello world!</p>;
+}
 
-// export default function HomePage() {
-//   return <p>Hello world!</p>;
-// }
+
