017e63a898b9708ff337a9b909e093b8b6248559
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2024-09-14 11:24:50 +0300

    LogOut Modal (Style / Markup)

--- src/components/Modals/LogOutModal/LogOutModal.jsx
+++ src/components/Modals/LogOutModal/LogOutModal.jsx
@@ -1,9 +1,18 @@
-import './LogOutModal.models.css'
+import "./LogOutModal.models.css";
 
 const LogOutModal = () => {
-    return (
-        <div> 
-            
-        </div>
-    )
-}
+  return (
+    <div className={css.logOutModalContainer}>
+      <div className={css.logOutModalTextContainer}>
+        <h2 className={css.logOutModalTitle}>Log out</h2>
+        <p className={css.logOutModalText}>Do you really want to leave?</p>
+      </div>
+      <div className={css.logOutModalBtnWrapper}>
+        <button className={css.logOutModalBtn}>Log out</button>
+        <button className={css.logOutModalCancelBtn}>Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default LogOutModal;
