fdcf46a3732fb29c929aed34959d6c7a1f33915b
LazarenkoNatalia <lazarenkon.n.v@gmail.com>
2024-09-15 20:52:09 +0300

    monthinfo add components

---
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -0,0 +1,26 @@
+import css from './MonthInfo.module.css';
+import CalendarPagination from './CalendarPagination/CalendarPagination.jsx';
+
+
+
+export default function MonthInfo() {
+
+    
+
+ 
+   
+    return (
+        <div className={css.container}>
+        {/* <div className={css.mainbox}> */}
+            {/* <div className={css.name}>Month</div> */}
+           
+                <CalendarPagination  />
+           
+          
+            {/* </div > */}
+            
+
+        </div>
+  )
+}
+
