09754210dc6a53c1e90c0f4ddc0285b6eaa60dad
Roxaneryz <roxane5666@yahoo.gr>
2024-09-16 01:26:35 +0300

    +

--- src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
@@ -5,8 +5,10 @@ const AdvantagesSection = () => {
   return (
     <div className={css.img}>
       <div className={css.infoStaticsBtn}>
-        <button className={css.habitbtn}>Habit drive</button>
+        <button className={css.habitbtn}><span className={css.circle}></span> Habit drive</button>
         <button className={css.statistics}>View statistics</button>
+      </div>
+      <div className={css.ratebtn}>
         <button className={css.rate}>Personal rate setting</button>
       </div>
     </div>
