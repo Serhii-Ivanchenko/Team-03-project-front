87af1803a53d4085b7410d56063e70f023aa3dea
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-09-16 16:24:53 +0300

    googleLogin

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -1,5 +1,6 @@
 import css from "./GoogleBtn.module.css";
 import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
 
 const Button = ({ children, type = "button", onClick, ...props }) => {
   return (
@@ -9,13 +10,33 @@ const Button = ({ children, type = "button", onClick, ...props }) => {
   );
 };
 
-const GoogleBtn = ({ context, onClick }) => {
+const GoogleBtn = ({ context }) => {
+  const handleGoogleLogin = async () => {
+    try {
+      const response = await axios.get("/auth/google/get-oauth-url");
+      const { url } = response.data.data;
+      // Перенаправляем пользователя на страницу авторизации Google
+      window.location.href = url;
+    } catch (error) {
+      console.error("Ошибка при получении URL для Google OAuth:", error);
+    }
+  };
+
   return (
-    <Button className={css.google_btn} onClick={onClick}>
+    <Button className={css.google_btn} onClick={handleGoogleLogin}>
       <FcGoogle className={css.icon_google} />
       {context}
     </Button>
   );
 };
+
+// const GoogleBtn = ({ context, onClick }) => {
+//   return (
+//     <Button className={css.google_btn} onClick={onClick}>
+//       <FcGoogle className={css.icon_google} />
+//       {context}
+//     </Button>
+//   );
+// };
 
 export default GoogleBtn;
