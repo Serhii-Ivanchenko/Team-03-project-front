90c21b601c300dc3e914ca5d79b7c592a771394d
Roxaneryz <roxane5666@yahoo.gr>
2024-09-16 20:15:30 +0300

    +

--- src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
@@ -1,11 +1,20 @@
 import css from "./AdvantagesSection.module.css";
 
-
 const AdvantagesSection = () => {
   return (
     <div className={css.img}>
+      {/* <span className={css.customers}>
+        
+        <img src={.} alt="Heart" />
+        <img src={} alt="Star" />
+        <img src={} alt="Check" />
+        Our happy customers
+      </span> */}
+      {/* <span className={css.happy}>happy</span> */}
       <div className={css.infoStaticsBtn}>
-        <button className={css.habitbtn}><span className={css.circle}></span> Habit drive</button>
+        <button className={css.habitbtn}>
+          <span className={css.circle}></span> Habit drive
+        </button>
         <button className={css.statistics}>View statistics</button>
       </div>
       <div className={css.ratebtn}>
