5e42272603165a5b7bee5a1ca55bed9f974e7d5f
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-09-16 22:29:21 -1000

    render monthInfo component and fixed userBarPopover position

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,12 +1,14 @@
-import DailyInfo from "../DailyInfo/DailyInfo"
-import UserPanel from "../UserPanel/UserPanel"
-import styles from "./WaterDetailedInfo.module.css"
+import DailyInfo from "../DailyInfo/DailyInfo";
+import UserPanel from "../UserPanel/UserPanel";
+import MonthInfo from "../MonthInfo/MonthInfo";
+import styles from "./WaterDetailedInfo.module.css";
 
 const WaterDetailedInfo = () => {
     return (
         <div className={styles.container}>
             <UserPanel />
             <DailyInfo />
+            <MonthInfo />
         </div>
     )
 }
