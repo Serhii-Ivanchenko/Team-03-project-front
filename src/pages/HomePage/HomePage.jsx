c7488caa422569a2eedc7568d415b3dcf09cc3d8
kulikp1 <pashakylik2@gmail.com>
2024-09-18 15:49:34 +0300

    create add water modal component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,14 +2,11 @@ import AdvantagesSection from "../../components/HomePage/AdvantagesSection/Advan
 import WelcomeSection from "../../components/HomePage/WelcomeSection/WelcomeSection.jsx";
 import css from "./HomePage.module.css";
 
-import AddWaterModal from "../../components/Modals/AddWaterModal/AddWaterModal";
-
 export default function HomePage() {
   return (
     <div>
       <WelcomeSection classname={css.wel} />
       <AdvantagesSection />
-      <AddWaterModal />
     </div>
   );
 }
