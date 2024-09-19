e0e91f9bda539bd3328b16644aabe578437e297d
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-09-18 23:24:13 -1000

    work with modal + button in component

--- src/components/AddWaterButton/AddWaterButton.jsx
+++ src/components/AddWaterButton/AddWaterButton.jsx
@@ -1,17 +1,36 @@
+import  { useState } from 'react';
 import styles from './AddWaterButton.module.css';
 import iconPlus from '../../assets/images/icons/icons.svg';
+import Modal from '../Modal/Modal';
+import EditWaterModal from '../Modals/EditWaterModal/EditWaterModal';
 
+const AddWaterButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-const AddWaterButton = ({ onClick }) => {
+  const openModal = () => {
+    console.log('Opening modal');
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    console.log('Closing modal');
+    setIsModalOpen(false);
+  };
+
   return (
-    <button className={styles.addWaterButton} onClick={onClick}>
-      <div className={styles.containerSvg}>
-      <svg className={styles.icon}>
-        <use href={`${iconPlus}#icon-plus`}></use>
-      </svg>
-      </div>
-      <span className={styles.text}>Add water</span>
-    </button>
+    <>
+      <button className={styles.addWaterButton} onClick={openModal}>
+        <div className={styles.containerSvg}>
+          <svg className={styles.icon}>
+            <use href={`${iconPlus}#icon-plus`}></use>
+          </svg>
+        </div>
+        <span className={styles.text}>Add water</span>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <EditWaterModal onClose={closeModal} />
+      </Modal>
+    </>
   );
 };
 
