130a148d3b9e1217b04facb94c190ab9a28eb83c
Roxaneryz <roxane5666@yahoo.gr>
2024-09-20 21:56:06 +0300

    screen 1440

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
 
   return (
     <div className={css.homePage}>
-      {/* <WelcomeSection /> */}
+      <WelcomeSection />
       <AdvantagesSection />
     </div>
   );
