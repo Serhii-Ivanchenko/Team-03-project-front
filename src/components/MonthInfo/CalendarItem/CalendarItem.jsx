6750c7da84e1795076b5f196bf6e2bc3fe7fcfcc
LazarenkoNatalia <lazarenkon.n.v@gmail.com>
2024-09-21 23:45:19 +0300

    monthinfo4
    add clicDate, disable click to increase month for current month, not clic day if day more then current day .

--- src/components/MonthInfo/CalendarItem/CalendarItem.jsx
+++ src/components/MonthInfo/CalendarItem/CalendarItem.jsx
@@ -1,25 +1,28 @@
+import { useDispatch } from "react-redux";
 import css from "./CalendarItem.module.css";
+import { changeActualDate } from '../../../redux/water/slice.js';
 
-export default function CalendarItem({ day, value, waterDailyNorm , isActiveDay }) {
-  
+export default function CalendarItem({date, day, value, waterDailyNorm , isActiveDay }) {
+  const dispatch = useDispatch();
   const valpercent = Math.round(value / waterDailyNorm * 100);
   const valday = parseInt(day);
-  let isColor = valpercent < 100 
-  
-    // let isActiveday = valday === new Date().getDate();
+  let isColor = valpercent < 100;
+  let isNotClick = new Date().toISOString().substring(0, 10) < date ?  true :  false;
+ 
   return (
     <li className={css.item}>
       
       <div className={css.infoday}>
-{isActiveDay && <button className={css.colordarkgrey}  >
+        {isActiveDay && <button className={css.colordarkgrey} onClick={() => dispatch(changeActualDate(date))}>
           {valday}
         </button>}
-{!isColor && !isActiveDay && <button className={css.colorwhite}  >
+         { !isActiveDay && isNotClick && <button className={isColor? css.colorgrey : css.colorwhite }  >
           {valday}
         </button>}
-{isColor && !isActiveDay && <button className={css.colorgrey}  >
+        { !isActiveDay && !isNotClick && <button className={isColor? css.colorgrey : css.colorwhite } onClick={() => dispatch(changeActualDate(date))} >
           {valday}
         </button>}
+     
        <p className={css.percent}>
           {valpercent}%
         </p>
