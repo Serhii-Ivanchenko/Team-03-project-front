c44cb63a0321dc4f9bb9ec076c85561855468dbd
kulikp1 <pashakylik2@gmail.com>
2024-09-22 20:44:06 +0300

    add validation for inputs, update dispatch add onClose...

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,14 @@
 import AdvantagesSection from "../../components/HomePage/AdvantagesSection/AdvantagesSection.jsx";
 import WelcomeSection from "../../components/HomePage/WelcomeSection/WelcomeSection.jsx";
+import AddWaterModal from "../../components/Modals/AddWaterModal/AddWaterModal.jsx";
 import css from "./HomePage.module.css";
 
-
 export default function HomePage() {
-
   return (
     <div className={css.homePage}>
       <WelcomeSection />
       <AdvantagesSection />
+      <AddWaterModal />
     </div>
   );
 }
