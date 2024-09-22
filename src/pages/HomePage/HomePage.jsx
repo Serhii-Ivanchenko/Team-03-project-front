3351724a4d3d515342200ddd775d847e0956dfad
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-09-22 21:45:40 +0300

    Delete addwater from home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import AdvantagesSection from "../../components/HomePage/AdvantagesSection/AdvantagesSection.jsx";
 import WelcomeSection from "../../components/HomePage/WelcomeSection/WelcomeSection.jsx";
-import AddWaterModal from "../../components/Modals/AddWaterModal/AddWaterModal.jsx";
 import css from "./HomePage.module.css";
 
 export default function HomePage() {
@@ -8,7 +7,6 @@ export default function HomePage() {
     <div className={css.homePage}>
       <WelcomeSection />
       <AdvantagesSection />
-      <AddWaterModal />
     </div>
   );
 }
