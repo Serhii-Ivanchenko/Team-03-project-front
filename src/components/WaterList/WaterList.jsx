83f3b6741f9178522bcb4aa708b9a5aaa277c7f5
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-09-23 17:14:32 -1000

    add sort to render time on waterItem

--- src/components/WaterList/WaterList.jsx
+++ src/components/WaterList/WaterList.jsx
@@ -25,11 +25,22 @@ const WaterList = () => {
     };
   }, []);
 
+  // Функція для перетворення часу у формат, який можна сортувати
+  const convertTimeToMinutes = (timeString) => {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+
+  // Сортуємо waterItems за часом
+  const sortedWaterItems = [...waterItems].sort((a, b) => {
+    return convertTimeToMinutes(a.time) - convertTimeToMinutes(b.time);
+  });
+
   return (
     <div className={styles.containerWrapper}>
       <ul ref={containerRef} className={styles.container}>
-        {waterItems.length > 0 ? (
-          waterItems.map((item) => (
+        {sortedWaterItems.length > 0 ? (
+          sortedWaterItems.map((item) => (
             <WaterItem key={item.id} itemId={item.id} value={item.value} time={item.time} />
           ))
         ) : (
