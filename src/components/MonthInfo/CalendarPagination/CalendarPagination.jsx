5c46b9d2b292046ecfd2564d7448741f83e6172a
Serhii-Ivanchenko <150422416+Serhii-Ivanchenko@users.noreply.github.com>
2024-09-25 14:48:37 +0300

    Update CalendarPagination.jsx

--- src/components/MonthInfo/CalendarPagination/CalendarPagination.jsx
+++ src/components/MonthInfo/CalendarPagination/CalendarPagination.jsx
@@ -85,7 +85,7 @@ export default function CalendarPagination() {
   return (
     <div className={css.containerpagin}>
       <div className={css.mainbox}>
-        <p className={css.name}>{isCalendar ? {t("month")} : "Statistics"}</p>
+        <p className={css.name}>{isCalendar ? "Month" : "Statistics"}</p>
         <div className={css.mainboxpagination}>
           <div className={css.boxpagination}>
             <button className={css.iconstep} onClick={handleClickLeft}>
